67447cd24ed7f0b2784d1c0323b2ca7bd1b9312c
macbook <macbook@macbooks-MacBook-Pro.local>
2022-07-20 14:45:22 +0700

    refactor: Integrate with serve

--- frontend/docker-etc/node/start.sh
+++ frontend/docker-etc/node/start.sh
@@ -6,8 +6,9 @@ npm install
 
 # Run next
 if [ "$NODE_ENV" = "production" ]; then
-  # npm run build
-  pm2-runtime npm -- start --omit=dev
+  npm run build
+  npm install -g serve
+  pm2-runtime serve -s build -l 3000
 else
   pm2-runtime npm -- start
 fi
