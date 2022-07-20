f736bbc2da719757065daea0b769ff7fbd6f8f0a
macbook <macbook@macbooks-MacBook-Pro.local>
2022-07-20 18:46:18 +0700

    refactor: Add --omit=dev in npm install

--- frontend/docker-etc/node/start.sh
+++ frontend/docker-etc/node/start.sh
@@ -1,15 +1,16 @@
-# Run npm install
-npm install
-
 # Tail node
 # tail -f /dev/null
 
 # Run server
 if [ "$NODE_ENV" = "production" ]; then
+  # Run npm install
+  npm install --omit=dev
   npm run build
   npm install -location=global serve
   serve -s build -l 3000
 else
+  # Run npm install
+  npm install
   npm install --location=global pm2
   pm2-runtime npm -- start
 fi
